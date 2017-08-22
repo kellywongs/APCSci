1ae08668989b9227738a6c402b30e92351af8633
Kelly Wong <gravitybee@icloud.com>
2017-08-22 09:25:07 -0700

    8/22/17 classwork
    created flowergatherer project in which flowers = cactuses bc i like cactuses
    
    private int called stepcounter. one of these days i'll remember how to do that w/o consulting old code

---
+++ Code/flower gatherer/Flower.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Flower here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Flower extends Actor
+{
+    /**
+     * Act - do whatever the Flower wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    private int stepCounter = 0;
+    
+    public void act() 
+    {
+       
+    }    
+}
