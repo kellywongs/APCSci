3259bce3e3eb4ff053295fb4dd38d590f642cafe
Kelly Wong <gravitybee@icloud.com>
2017-09-14 09:42:09 -0700

    test day!!!
    fun

--- Code/Greenfoot/plant.java
+++ Code/Greenfoot/plant.java
@@ -12,13 +12,35 @@ public class Plant extends Actor
      * Act - do whatever the plant wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    int bounce;
+    
+    public Plant()
+    {
+        bounce = 60;
+    }
+    
     public void act() 
     {
-        this.move(-5);
-        if(this.isAtEdge() == true) 
-        {
-            this.turn(40);
-        };
+        
+        World w = getWorld();
+        
+        if (this.getX() > (0 + bounce) &&
+            this.getX() < (w.getWidth() - bounce) &&
+            this.getY() > (0 + bounce) &&
+            this.getY() < (w.getHeight() - bounce))
+            {
+                this.move(5);
+            }
+            else {
+                this.turn(180);
+                this.move(5);
+            }
+        
+        // if(this.isAtEdge() == true) 
+        // {
+            // this.turn(40);
+        // };
         // if(this.isTouching(java.lang.Class<null> cls))
         // {
             // this.turn(180);
