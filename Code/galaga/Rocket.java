639cf43f4e1ad25105ba975aa44ce598bf3e017a
Kelly Wong <gravitybee@icloud.com>
2017-09-18 09:30:12 -0700

    startted galaga thing
    jfdkalsajkflsdlfjsdakf jksdlafdjl;ska

---
+++ Code/galaga/Rocket.java
@@ -0,0 +1,43 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Rocket here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Rocket extends Actor
+{
+    int direction;
+    
+    public Rocket()
+    {
+        this.setRotation(270);
+        direction = 180;
+    }
+    
+    public void act() 
+    {
+        this.navigation();
+    }    
+    
+    public void navigation()
+    {
+        if (Greenfoot.isKeyDown("left"))
+        {  
+            
+            this.setRotation(180);
+            this.move(3);
+            this.setRotation(270);
+        }
+        
+        if (Greenfoot.isKeyDown("right"))
+        {  
+            
+            this.setRotation(0);
+            this.move(3);
+            this.setRotation(270);
+        }
+        
+    }
+}
