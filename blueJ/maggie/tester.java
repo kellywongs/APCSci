65632ee71fe341a02630378f3e1d1d04e08e142d
Kelly Wong <gravitybee@icloud.com>
2017-11-07 09:29:32 -0800

    i something you
    Im actually paying attention again wow

--- blueJ/maggie/tester.java
+++ blueJ/maggie/tester.java
@@ -4,23 +4,31 @@ public class tester
 {
     public static void main(String[] args)
     {
-        while (1==1) 
-        {
-            Scanner thing = new Scanner(System.in);
-            System.out.print("input some text: ");
-            String text = thing.nextLine().toLowerCase();
-            System.out.println(text);
+        // while (1==1) 
+        // {
+            // Scanner thing = new Scanner(System.in);
+            // System.out.print("input some text: ");
+            // String text = thing.nextLine().toLowerCase();
+            // System.out.println(text);
             
-            String let = String.valueOf(text.charAt(0));
+            // String let = String.valueOf(text.charAt(0));
             
-            System.out.println(let.compareTo("z"));
+            // System.out.println(let.compareTo("z"));
             
-            if (let.compareTo("z") > -26 && let.compareTo("z") < 1)
-            {
-                System.out.println("letter");
-            } else {
-                System.out.println("symbol");
-            }
-        }
+            // if (let.compareTo("z") > -26 && let.compareTo("z") < 1)
+            // {
+                // System.out.println("letter");
+            // } else {
+                // System.out.println("symbol");
+            // }
+        // }
+        
+        String g = "midnight";
+        String s = "the dog howls at midnight";
+        
+        if (s.substring(s.lastIndexOf(" "), s.length()).equals(g))
+        { System.out.println("ye"); } else { System.out.println("no"); }
+        
+        System.out.println(s.substring(s.lastIndexOf(" "), s.length()));
     }
 }
