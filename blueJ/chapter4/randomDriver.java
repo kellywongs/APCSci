e1183d8b7e45d6aadc9e0ac0a1f95766d6c0ced7
Kelly Wong <gravitybee@icloud.com>
2017-11-28 09:28:38 -0800

    gothib
    save net  neutraliity

--- blueJ/chapter4/randomDriver.java
+++ blueJ/chapter4/randomDriver.java
@@ -7,27 +7,11 @@
  */
 public class randomDriver
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class randomDriver
-     */
-    public randomDriver()
+    public static void main() {
+        randomizer rand = new randomizer();
+    while (1 == 1)
     {
-        // initialise instance variables
-        x = 0;
+        rand.randomize();
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+}
 }
--- blueJ/chapter4/randomizer.java
+++ blueJ/chapter4/randomizer.java
@@ -8,17 +8,17 @@ import java.util.Random;
 public class randomizer
 
 {
-    private int newRandomNumber, totalNumbers; 
-    private int frequency0 = 0;
-    private int frequency1 = 0;
-    private int frequency2 = 0;
-    private int frequency3 = 0;
-    private int frequency4 = 0;
-    private int frequency5 = 0;
-    private int frequency6 = 0;
-    private int frequency7 = 0;
-    private int frequency8 = 0;
-    private int frequency9 = 0;
+     int newRandomNumber, totalNumbers; 
+     int frequency0;
+     int frequency1;
+     int frequency2;
+     int frequency3;
+     int frequency4;
+     int frequency5;
+     int frequency6;
+     int frequency7;
+     int frequency8;
+     int frequency9;
     
     Random ran = new Random();
     
@@ -27,11 +27,24 @@ public class randomizer
         newRandomNumber = 0;
         totalNumbers = 0;
         
+        frequency0 = 0;
+        frequency1 = 0;
+        frequency2 = 0;
+        frequency3 = 0;
+        frequency4 = 0;
+        frequency5 = 0;
+        frequency6 = 0;
+        frequency7 = 0;
+        frequency8 = 0;
+        frequency9 = 0;
     }
 
-    public int randomize()
+    public void randomize()
     {
-        newRandomNumber = ran.nextInt(9);
+        newRandomNumber = ran.nextInt(10);
+        System.out.println(totalNumbers); 
+        System.out.println(newRandomNumber); 
+        totalNumbers += 1;
         
         if      (newRandomNumber == 0) { frequency0 += 1; }
         else if (newRandomNumber == 1) { frequency1 += 1; }
@@ -43,5 +56,18 @@ public class randomizer
         else if (newRandomNumber == 7) { frequency7 += 1; }
         else if (newRandomNumber == 8) { frequency8 += 1; }
         else if (newRandomNumber == 9) { frequency9 += 1; }
+       
+        
+        System.out.println("0: " + (frequency0 * 100 /totalNumbers) + "%");
+        System.out.println("1: " + (frequency1 * 100 /totalNumbers) + "%");
+        System.out.println("2: " + (frequency2 * 100 /totalNumbers) + "%");
+        System.out.println("3: " + (frequency3 * 100 /totalNumbers) + "%");
+        System.out.println("4: " + (frequency4 * 100 /totalNumbers) + "%");
+        System.out.println("5: " + (frequency5 * 100 /totalNumbers) + "%");
+        System.out.println("6: " + (frequency6 * 100 /totalNumbers) + "%");
+        System.out.println("7: " + (frequency7 * 100 /totalNumbers) + "%");
+        System.out.println("8: " + (frequency8 * 100 /totalNumbers) + "%");
+        System.out.println("9: " + (frequency9 * 100 /totalNumbers) + "%");
+        
     }
 }
